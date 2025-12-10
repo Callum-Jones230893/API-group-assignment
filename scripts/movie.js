e665a4da37554110c1bbdb7d461fabe73dbc6c38
Nathalie <nathaliebuss21@gmail.com>
2025-12-10 14:34:57 +0100

    adding more js.

--- scripts/movie.js
+++ scripts/movie.js
@@ -1,22 +1,23 @@
-const apiKey = "87a84deb"
-const apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=87a84deb"
+const apiUrl = "https://jsonfakery.com/movies/random";
+const movieDisplay = document.getElementById("button")
 
 const findMovie = async () => {
-    let searchValue = searchInput.value
-    searchValue = searchValue.trim()
+  try {
+    const response = await fetch(apiUrl);
 
-    const movieUrl = `${apiUrl}?apikey=${apiKey}&t=${searchValue}`
-
-    try {
-        const response = await (fetch(url))
-    
-    if(!response.ok) {
-        throw new Error (`Network response was not ok`)
-        }
-    const movie = await response.json()
-
-    if (movie.Response === "True") {
-        console.log("movie".movie)
+    if (!response.ok) {
+      throw new Error(`Network response was not ok`);
     }
-    }
+    const movie = await response.json();
+    console.log(movie.original_title);
+  } catch (error) {
+    console.log("Error", error);
+    throw error;
+  }
+};
+
+const showMovie = () => {
+   movieDisplay.innerHTML = ""
 }
+
+findMovie();
