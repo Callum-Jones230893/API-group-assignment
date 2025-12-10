0282d67e70d7f634014c66067a845378c96b4765
Callum Jones <call_jones2@hotmail.com>
2025-12-10 15:41:28 +0100

    added dom manipulation with ingredients and measurements

--- scripts/drink.js
+++ scripts/drink.js
@@ -1,5 +1,5 @@
 const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-const results = document.querySelector(".result");
+const results = document.querySelector(".results");
 
 const findDrink = async () => {
   try {
@@ -8,22 +8,30 @@ const findDrink = async () => {
     if (!response.ok) {
       throw new Error("Please try again later.")
 
-    } else {
-      results.textContent = ""
+    } 
+      results.textContent = "Loading"
 
       const data = await response.json()
-      console.log(data)
-      return (data)
-    }
-  } catch (error) {
-    results.textContent = ""
+      const drink = data.drinks[0]
+      results.innerHTML = `<h2>${drink.strDrink}</h2>`
+      
+      let ul = document.createElement("ul")
+
+      Object.keys(drink).forEach(key => {
+        if (key.startsWith("strIngredient") && drink[key]) {
+          let mainKey = key.replace("strIngredient", "")
+          let drinkIngredient = drink[key]
+          let drinkMeasurement = drink[`strMeasure${mainKey}`]
+          let drinkInfo = document.createElement("li")
+          drinkInfo.textContent = `${drinkMeasurement}${drinkIngredient ? `${drinkIngredient}` : ""}`
+          ul.appendChild(drinkInfo)
+        } 
+      })
+    results.appendChild(ul)
+
+  } catch (error){
+    results.innerHTML = `<div class ="unlucky"> unlucky </div>`
   }
 }
 
-findDrink()
-
-Object.keys(drink).forEach(key => {
-  if (key.startsWith("strIngredient") && drink[key]) {
-    let ingredient = document.createElement("li")
-    ingredient.
- }})
+findDrink() 
