6303a758b7d918e32b2041e2d3c096a9ab508f59
Callum Jones <call_jones2@hotmail.com>
2025-12-10 16:00:41 +0100

    fixed formatting (I think)

--- scripts/drink.js
+++ scripts/drink.js
@@ -1,5 +1,5 @@
 const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-const results = document.querySelector(".results");
+const results = document.querySelector("#drink");
 
 const findDrink = async () => {
   try {
@@ -9,29 +9,29 @@ const findDrink = async () => {
       throw new Error("Please try again later.")
 
     } 
-      results.textContent = "Loading"
+    results.textContent = "Loading"
 
-      const data = await response.json()
-      const drink = data.drinks[0]
-      results.innerHTML = `
-      <h2>${drink.strDrink}</h2>
-      <img src="${drink.strDrinkThumb}">`
-      
-      let ul = document.createElement("ul")
+    const data = await response.json()
+    const drink = data.drinks[0]
+    results.innerHTML = `
+    <h2>${drink.strDrink}</h2>
+    <img src="${drink.strDrinkThumb}">`
+    
+    let ingredientList = document.createElement("ul")
 
-      Object.keys(drink).forEach(key => {
-        if (key.startsWith("strIngredient") && drink[key]) {
-          let mainKey = key.replace("strIngredient", "")
-          let drinkIngredient = drink[key]
-          let drinkMeasurement = drink[`strMeasure${mainKey}`]
-          let drinkInfo = document.createElement("li")
+    Object.keys(drink).forEach(key => {
+      if (key.startsWith("strIngredient") && drink[key]) {
+        let mainKey = key.replace("strIngredient", "")
+        let drinkIngredient = drink[key]
+        let drinkMeasurement = drink[`strMeasure${mainKey}`]
+        let drinkInfo = document.createElement("li")
 
-          drinkInfo.textContent = `${drinkMeasurement}${drinkIngredient ? `${drinkIngredient}` : ""}`
-          ul.appendChild(drinkInfo)
-        } 
-      })
-    results.appendChild(ul)
-
+        drinkInfo.textContent = `${drinkMeasurement ? `${drinkMeasurement}` : ""}${drinkIngredient ? `${drinkIngredient}` : ""}`
+        ingredientList.appendChild(drinkInfo)
+      } 
+    })
+    results.appendChild(ingredientList)
+    
   } catch (error){
     results.innerHTML = `<div> Please try again.</div>`
   }
@@ -42,3 +42,6 @@ let drinkBtn = document.querySelector("#drink-button")
 drinkBtn.addEventListener(`click`, () => {
   findDrink()
 })
+
+//add category to search for if we add search function?
+//add info if alcoholic or not?
