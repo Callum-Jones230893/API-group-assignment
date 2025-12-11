b87b0c874c94b97e046e32cd2a9cc444a7bc10b4
claudiacarion <claudia.carion@gmail.com>
2025-12-11 11:49:19 +0100

    add functionality, no styling

---
+++ scripts/food.js
@@ -0,0 +1,52 @@
+const fetchRecipes = () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Error!");
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(`Food API: `, data);
+
+      const image = document.querySelector("#food-image");
+      image.src = data.meals[0].strMealThumb;
+      image.height = "150";
+
+      document.querySelector("#food-card__title").textContent = data.meals[0].strMeal;
+      document.querySelector("#food-origin").textContent = `Culinary origin: ${data.meals[0].strArea}`;
+
+      const meal = data.meals[0];
+      const ingredientsList = [];
+
+      Object.keys(meal).forEach(key => {
+        if (key.startsWith("strIngredient") && meal[key]) {
+          const ingredient = meal[key];
+          const index = key.replace("strIngredient", "");
+          const measurement = meal[`strMeasure${index}`] || "";
+
+          ingredientsList.push(`${ingredient}: ${measurement}`);
+        }
+      });
+
+      const instructions = document.createElement("div");
+      instructions.innerHTML = `
+      <ul class="info" id="ingredients-list"></ul>
+      <div class="info" id="steps">${data.meals[0].strInstructions}</div>
+      `;
+      document.querySelector("#recipe").appendChild(instructions);
+
+      ingredientsList.forEach(item => {
+        let listItem = document.createElement("li");
+        listItem.textContent = item;
+        document.querySelector("#ingredients-list").appendChild(listItem);
+      });
+    });
+};
+
+fetchRecipes();
+
+const readMoreBtn = document.querySelector("#read-more-btn");
+readMoreBtn.onclick = () => {
+  document.querySelector("#recipe").classList.toggle("hide");
+};
