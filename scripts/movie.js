7b6cf0534ccf11fe12fc464d44745a3e100b40e8
Nathalie <nathaliebuss21@gmail.com>
2025-12-12 10:48:51 +0100

    fetching poster form API.

--- scripts/movie.js
+++ scripts/movie.js
@@ -1,5 +1,5 @@
 const apiUrl = "https://jsonfakery.com/movies/random";
-const movieDisplay = document.getElementById("button")
+const movieDisplay = document.getElementById("movie-button");
 
 const findMovie = async () => {
   try {
@@ -8,16 +8,50 @@ const findMovie = async () => {
     if (!response.ok) {
       throw new Error(`Network response was not ok`);
     }
+
+    const imgURL =
+      "https://image.tmdb.org/t/p/original/nBLPIpReSatt1zcgVzSVzq5e581.jpg";
+    let imageDisplay = document.getElementById("img");
+
     const movie = await response.json();
     console.log(movie.original_title);
+
+    let movieTitle = document.createElement("h2");
+    movieTitle.textContent = movie.original_title;
+    document.body.appendChild(movieTitle);
+
+    let synopsis = document.createElement("p");
+    synopsis.textContent = movie.overview;
+    document.body.appendChild(synopsis);
+
+    let casts = movie.casts;
+    casts.forEach((cast) => {
+      console.log(cast.name);
+      casts.document.createElement("p");
+      casts.textContent = movie.casts;
+      document.body.appendChild(casts);
+    });
+
+    console.log(casts);
   } catch (error) {
     console.log("Error", error);
     throw error;
   }
 };
 
-const showMovie = () => {
-   movieDisplay.innerHTML = ""
-}
-
 findMovie();
+
+/*const showMovie = () => {
+  movieDisplay.innerHTML = "";
+
+  const title = movie.original_title;
+  const synopsis = movie.overview;
+  const poster = movie.poster_path;
+
+  const casts = movie.casts; //have to do it in a foreach-loop
+  casts.forEach((casts) => {
+    console.log(movie.casts);
+  });
+};
+
+*/
