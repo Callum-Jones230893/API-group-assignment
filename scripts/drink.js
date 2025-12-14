4b11d8bf177caf85025ae50e719aa5cb80a734b3
Callum Jones <call_jones2@hotmail.com>
2025-12-14 14:07:19 +0100

    cleaned up code abit

--- scripts/drink.js
+++ scripts/drink.js
@@ -1,22 +1,27 @@
 const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 const results = document.querySelector("#drink");
+const readMoreBtn = document.querySelector("#read-more-btn");
+const ingredientDiv = document.querySelector("#ingredient");
+const drinkBtn = document.querySelector("#drink-button")
 
 const findDrink = async () => {
   try {
     const response = await fetch(url)
-
+    
     if (!response.ok) {
       throw new Error("Please try again later.")
-
     } 
-    results.textContent = "Loading"
 
     const data = await response.json()
     const drink = data.drinks[0]
+    readMoreBtn.classList.remove("hide");
+
     results.innerHTML = `
     <h2>${drink.strDrink}</h2>
-    <h3>${drink.strCategory}, ${drink.strAlcoholic}<h3>
+    <h3>${drink.strCategory}, ${drink.strAlcoholic}</h3>
     <img src="${drink.strDrinkThumb}">`
+
+    ingredientDiv.classList.add("hide")
     
     let ingredientList = document.createElement("ul")
 
@@ -27,19 +32,23 @@ const findDrink = async () => {
         let drinkMeasurement = drink[`strMeasure${ingredientIndex}`]
         let drinkInfo = document.createElement("li")
 
-        drinkInfo.textContent = `${drinkMeasurement ? `${drinkMeasurement}` : ""}${drinkIngredient ? `${drinkIngredient}` : ""}`
+        drinkInfo.textContent = `${drinkMeasurement} : ${drinkIngredient}`
         ingredientList.appendChild(drinkInfo)
       } 
     })
-    results.appendChild(ingredientList)
 
-  } catch (error){
+    ingredientDiv.appendChild(ingredientList); 
+
+  } catch (error) {
     results.innerHTML = `<div>Please try again.</div>`
   }
 }
 
-let drinkBtn = document.querySelector("#drink-button")
-
 drinkBtn.addEventListener(`click`, () => {
   findDrink()
 })
+
+readMoreBtn.addEventListener("click", () => {
+  ingredientDiv.classList.toggle("hide");
+})
+
